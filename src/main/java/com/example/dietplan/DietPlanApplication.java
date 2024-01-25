cb1d757916e68c518197dacb8fa9ff497449041b
TaewoooongHeo01 <chamjoeun0111@gmail.com>
2024-01-25 20:17:21 +0900

    first commit

---
+++ src/main/java/com/example/dietplan/DietPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietPlanApplication.class, args);
+    }
+
+}
